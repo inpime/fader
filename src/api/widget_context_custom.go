01fdb112267aeb6fead5fb8fb9588b742a98ef67
G B <egor.workroom@gmail.com>
2016-06-20 23:24:40 +0600

    add helpful function for context

--- src/api/widget_context_custom.go
+++ src/api/widget_context_custom.go
@@ -1,6 +1,8 @@
 package api
 
 import (
+	"bytes"
+	"io"
 	"net/http"
 	"utils"
 )
@@ -88,4 +90,44 @@ func (c ContextWrap) BindFormToMap(fieldNames ...string) utils.M {
 		m.Set(name, c.FormValue(name))
 	}
 	return m
+}
+
+// -----------------------------------
+// Helpful functions for user form
+// -----------------------------------
+
+// FileData the data file
+type FileData struct {
+	Data        []byte
+	Name        string
+	ContentType string
+}
+
+func (f FileData) IsEmpty() bool {
+	return len(f.Data) == 0
+}
+
+// FormFileData file data from the form
+func (c ContextWrap) FormFileData(name string) *FileData {
+	file, err := c.FormFile(name)
+	if err != nil {
+		return &FileData{}
+	}
+
+	// fileName := file.Filename
+	fileOpen, _ := file.Open()
+	defer fileOpen.Close()
+
+	fileContent := bytes.NewBuffer([]byte{})
+	_, err = io.Copy(fileContent, fileOpen)
+
+	if err != nil {
+		return &FileData{}
+	}
+
+	return &FileData{
+		Data:        fileContent.Bytes(),
+		Name:        file.Filename,
+		ContentType: file.Header.Get("Content-Type"),
+	}
 }
