eed57ba07499ea5de3c86ce257d61fa7ebd58ebd
G B <egor.workroom@gmail.com>
2016-06-24 11:37:35 +0600

    add function URLQuery

--- src/api/pongo2_globalcustoms.go
+++ src/api/pongo2_globalcustoms.go
@@ -4,12 +4,15 @@ import (
 	"github.com/Sirupsen/logrus"
 	"github.com/flosch/pongo2"
 	"github.com/inpime/dbox"
+	"net/url"
 	"store"
 	"strings"
 	"utils"
 )
 
 func pongo2InitGlobalCustoms() {
+
+	var emptyURL, _ = url.Parse("")
 
 	// SearchFiles search files in bucket
 	// (the request is formed in buildSearchQueryFilesByBycket)
@@ -69,10 +72,51 @@ func pongo2InitGlobalCustoms() {
 		return pongo2.AsValue(file)
 	}
 
+	tpls.Globals["URLQuery"] = func(args ...*pongo2.Value) *pongo2.Value {
+		if len(args) == 0 {
+			return pongo2.AsValue(emptyURL)
+		}
+
+		_url, ok := args[0].Interface().(*url.URL)
+
+		if !ok {
+			logrus.Warningf("not expected type %T, want '*url.URL'", args[0].Interface())
+
+			return pongo2.AsValue(emptyURL)
+		}
+
+		if (len(args)-1)%2 != 0 {
+			logrus.Warningf("args expected in multiples of two, want %d", len(args)-1)
+			return pongo2.AsValue(emptyURL)
+		}
+
+		queryParams := args[1:]
+		urlQueryValues := _url.Query()
+
+		for i := 0; i < len(queryParams); i += 2 {
+			urlQueryValues.Add(queryParams[i].String(), queryParams[i+1].String())
+		}
+
+		_url.RawQuery = urlQueryValues.Encode()
+
+		return pongo2.AsValue(_url)
+	}
+
 	// builds the path part of the URL
 	tpls.Globals["URL"] = func(args ...*pongo2.Value) *pongo2.Value {
 		if len(args) == 0 {
-			return pongo2.AsValue("")
+			return pongo2.AsValue(emptyURL)
+		}
+
+		route := router.Get(args[0].String())
+
+		if route == nil {
+			return pongo2.AsValue(emptyURL)
+		}
+
+		if (len(args)-1)%2 != 0 {
+			logrus.Warningf("args expected in multiples of two, want %d", len(args)-1)
+			return pongo2.AsValue(emptyURL)
 		}
 
 		stringArgs := []string{}
@@ -81,21 +125,15 @@ func pongo2InitGlobalCustoms() {
 			stringArgs = append(stringArgs, arg.String())
 		}
 
-		route := router.Get(args[0].String())
-
-		if route == nil {
-			return pongo2.AsValue("")
-		}
-
-		url, err := route.URLPath(stringArgs...)
+		_url, err := route.URLPath(stringArgs...)
 
 		if err != nil {
 			logrus.WithError(err).Warning("build url")
 
-			return pongo2.AsValue("")
+			return pongo2.AsValue(emptyURL)
 		}
 
-		return pongo2.AsValue(url.String())
+		return pongo2.AsValue(_url)
 	}
 
 	// Load load file by name
