d23fa6e3f2f7db3c3d520164df78de5edb8cabae
G B <egor.workroom@gmail.com>
2016-07-19 17:45:47 +0600

    updated data merge utils

--- utils/appendorreplace.go
+++ utils/appendorreplace.go
@@ -1,111 +1,116 @@
 package utils
 
-import (
-	"errors"
-	// "github.com/inpime/fader/utils/sdata"
-	// "log"
-	"reflect"
-)
+// import (
+// 	"errors"
+// 	// "github.com/inpime/fader/utils/sdata"
+// 	// "log"
+// 	// "github.com/imdario/mergo"
+// 	"reflect"
+// )
 
-func resolveValues(dst, src interface{}) (vdst reflect.Value, vsrc reflect.Value, err error) {
-	if dst == nil || src == nil {
-		err = errors.New("nil args")
-		return
-	}
+// func resolveValues(dst, src interface{}) (vdst reflect.Value, vsrc reflect.Value, err error) {
+// 	if dst == nil || src == nil {
+// 		err = errors.New("nil args")
+// 		return
+// 	}
 
-	// destination
+// 	// destination
 
-	vdst = reflect.ValueOf(dst)
+// 	vdst = reflect.ValueOf(dst)
 
-	if vdst.Kind() == reflect.Ptr {
-		vdst = vdst.Elem()
-	}
+// 	if vdst.Kind() == reflect.Ptr {
+// 		vdst = vdst.Elem()
+// 	}
 
-	if vdst.Kind() != reflect.Struct &&
-		vdst.Kind() != reflect.Slice &&
-		vdst.Kind() != reflect.Map {
+// 	if vdst.Kind() != reflect.Struct &&
+// 		vdst.Kind() != reflect.Slice &&
+// 		vdst.Kind() != reflect.Map {
 
-		err = errors.New("not supported")
+// 		err = errors.New("not supported")
 
-		return
-	}
+// 		return
+// 	}
 
-	// source
+// 	// source
 
-	vsrc = reflect.ValueOf(src)
+// 	vsrc = reflect.ValueOf(src)
 
-	if vsrc.Kind() == reflect.Ptr {
-		vsrc = vsrc.Elem()
-	}
+// 	if vsrc.Kind() == reflect.Ptr {
+// 		vsrc = vsrc.Elem()
+// 	}
 
-	if vdst.Type() != vsrc.Type() {
-		err = errors.New("diff args types")
-		return
-	}
+// 	if vdst.Type() != vsrc.Type() {
+// 		err = errors.New("diff args types")
+// 		return
+// 	}
 
-	return
-}
+// 	return
+// }
 
-func appendOrReplace(vdst, vsrc reflect.Value) error {
+// func appendOrReplace(vdst, vsrc reflect.Value) error {
 
-	switch vdst.Kind() {
-	case reflect.Struct:
-		for i := 0; i < vdst.NumField(); i++ {
-			if err := appendOrReplace(vdst.Field(i), vsrc.Field(i)); err != nil {
-				return err
-			}
-		}
-	case reflect.Map:
-		if !isEmptyValue(vdst) && !isEmptyValue(vsrc) {
-			for _, key := range vsrc.MapKeys() {
-				vdst.SetMapIndex(key, vsrc.MapIndex(key)) // if error "panic: assignment to entry in nil map".
-				// because map not init, please make(map[...]...)
-			}
-		}
-	case reflect.Slice:
-		for i := 0; i < vsrc.Len(); i++ {
-			vdst.Set(reflect.Append(vdst, vsrc.Index(i)))
-		}
-	case reflect.Ptr:
-		if isEmptyValue(vsrc) {
-			return nil
-		}
+// 	switch vdst.Kind() {
+// 	case reflect.Struct:
+// 		for i := 0; i < vdst.NumField(); i++ {
+// 			if err := appendOrReplace(vdst.Field(i), vsrc.Field(i)); err != nil {
+// 				return err
+// 			}
+// 		}
+// 	case reflect.Map:
+// 		if !isEmptyValue(vdst) && !isEmptyValue(vsrc) {
+// 			for _, key := range vsrc.MapKeys() {
+// 				vdst.SetMapIndex(key, vsrc.MapIndex(key)) // if error "panic: assignment to entry in nil map".
+// 				// because map not init, please make(map[...]...)
+// 			}
+// 		}
+// 	case reflect.Slice:
+// 		for i := 0; i < vsrc.Len(); i++ {
+// 			vdst.Set(reflect.Append(vdst, vsrc.Index(i)))
+// 		}
+// 	case reflect.Ptr:
+// 		if isEmptyValue(vsrc) {
+// 			return nil
+// 		}
 
-		return appendOrReplace(vdst.Elem(), vsrc.Elem())
-	default:
-		if vdst.CanSet() && !isEmptyValue(vsrc) {
-			vdst.Set(vsrc)
-		}
-	}
+// 		return appendOrReplace(vdst.Elem(), vsrc.Elem())
+// 	default:
+// 		if vdst.CanSet() && !isEmptyValue(vsrc) {
+// 			vdst.Set(vsrc)
+// 		}
+// 	}
 
-	return nil
-}
+// 	return nil
+// }
 
 func AppendOrReplace(d, s interface{}) error {
-	vd, vs, err := resolveValues(d, s)
-
-	if err != nil {
-		return err
-	}
-
-	return appendOrReplace(vd, vs)
+	return Merge(d, s)
 }
 
-// From src/pkg/encoding/json.
-func isEmptyValue(v reflect.Value) bool {
-	switch v.Kind() {
-	case reflect.Array, reflect.Map, reflect.Slice, reflect.String:
-		return v.Len() == 0
-	case reflect.Bool:
-		return !v.Bool()
-	case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
-		return v.Int() == 0
-	case reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64, reflect.Uintptr:
-		return v.Uint() == 0
-	case reflect.Float32, reflect.Float64:
-		return v.Float() == 0
-	case reflect.Interface, reflect.Ptr:
-		return v.IsNil()
-	}
-	return false
-}
+// // func AppendOrReplace(d, s interface{}) error {
+// // 	vd, vs, err := resolveValues(d, s)
+
+// // 	if err != nil {
+// // 		return err
+// // 	}
+
+// // 	return appendOrReplace(vd, vs)
+// // }
+
+// // From src/pkg/encoding/json.
+// func isEmptyValue(v reflect.Value) bool {
+// 	switch v.Kind() {
+// 	case reflect.Array, reflect.Map, reflect.Slice, reflect.String:
+// 		return v.Len() == 0
+// 	case reflect.Bool:
+// 		return !v.Bool()
+// 	case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
+// 		return v.Int() == 0
+// 	case reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64, reflect.Uintptr:
+// 		return v.Uint() == 0
+// 	case reflect.Float32, reflect.Float64:
+// 		return v.Float() == 0
+// 	case reflect.Interface, reflect.Ptr:
+// 		return v.IsNil()
+// 	}
+// 	return false
+// }
--- utils/appendorreplace_test.go
+++ utils/appendorreplace_test.go
@@ -1,97 +1,149 @@
 package utils
 
-import (
-	"github.com/inpime/fader/utils/sdata"
-	"reflect"
-	"testing"
-)
+// import (
+// 	"github.com/inpime/fader/utils/sdata"
+// 	"reflect"
+// 	"testing"
+// )
 
-type simpleSubType struct {
-	A string
-}
+// type simpleSubType struct {
+// 	A string
+// }
 
-type simpleType struct {
-	A []string
-	B string
-	C []simpleSubType
-	D map[string]string
-	E map[string]simpleSubType
-	G *simpleSubType
-	F map[string]*simpleSubType
-	M *sdata.StringMap
-}
+// type simpleType struct {
+// 	A []string
+// 	B string
+// 	C []simpleSubType
+// 	D map[string]string
+// 	E map[string]simpleSubType
+// 	G *simpleSubType
+// 	F map[string]*simpleSubType
+// 	M *sdata.StringMap
+// }
 
-func TestAppendOrReplace(t *testing.T) {
-	s := simpleType{
-		[]string{"a", "b"},
-		"a",
-		[]simpleSubType{simpleSubType{"a"}, simpleSubType{"b"}},
-		map[string]string{"a": "b", "c": "d"},
-		map[string]simpleSubType{
-			"a": simpleSubType{"b"},
-			"c": simpleSubType{"d"},
-		},
-		&simpleSubType{"a"},
-		map[string]*simpleSubType{
-			"a": &simpleSubType{"b"},
-			"c": &simpleSubType{"d"},
-		},
-		sdata.NewStringMapFrom(map[string]interface{}{
-			"a":  "b",
-			"f1": sdata.NewStringMapFrom(map[string]interface{}{"a": "b"}),
-		}),
-	}
-	d := simpleType{
-		[]string{"b", "c"},
-		"b",
-		[]simpleSubType{simpleSubType{"c"}, simpleSubType{"d"}},
-		map[string]string{"a": "z", "c": "y", "e": "f"},
-		map[string]simpleSubType{
-			"a": simpleSubType{"z"},
-			"c": simpleSubType{"y"},
-			"e": simpleSubType{"f"},
-		},
-		&simpleSubType{"b"},
-		map[string]*simpleSubType{
-			"a": &simpleSubType{"z"},
-			"c": &simpleSubType{"y"},
-			"e": &simpleSubType{"f"},
-		},
-		sdata.NewStringMapFrom(map[string]interface{}{
-			"c":  "d",
-			"f1": sdata.NewStringMapFrom(map[string]interface{}{"c": "d"}),
-			"f2": sdata.NewStringMapFrom(map[string]interface{}{"a": "b"}),
-		}),
-	}
+// func TestAppendOrReplace(t *testing.T) {
+// 	s := simpleType{
+// 		[]string{"a", "b"},
+// 		"a",
+// 		[]simpleSubType{simpleSubType{"a"}, simpleSubType{"b"}},
+// 		map[string]string{"a": "b", "c": "d"},
+// 		map[string]simpleSubType{
+// 			"a": simpleSubType{"b"},
+// 			"c": simpleSubType{"d"},
+// 		},
+// 		&simpleSubType{"a"},
+// 		map[string]*simpleSubType{
+// 			"a": &simpleSubType{"b"},
+// 			"c": &simpleSubType{"d"},
+// 		},
+// 		sdata.NewStringMapFrom(map[string]interface{}{
+// 			"a":  "b",
+// 			"f1": sdata.NewStringMapFrom(map[string]interface{}{"a": "b"}),
+// 		}),
+// 	}
+// 	d := simpleType{
+// 		[]string{"b", "c"},
+// 		"b",
+// 		[]simpleSubType{simpleSubType{"c"}, simpleSubType{"d"}},
+// 		map[string]string{"a": "z", "c": "y", "e": "f"},
+// 		map[string]simpleSubType{
+// 			"a": simpleSubType{"z"},
+// 			"c": simpleSubType{"y"},
+// 			"e": simpleSubType{"f"},
+// 		},
+// 		&simpleSubType{"b"},
+// 		map[string]*simpleSubType{
+// 			"a": &simpleSubType{"z"},
+// 			"c": &simpleSubType{"y"},
+// 			"e": &simpleSubType{"f"},
+// 		},
+// 		sdata.NewStringMapFrom(map[string]interface{}{
+// 			"c":  "d",
+// 			"f1": sdata.NewStringMapFrom(map[string]interface{}{"c": "d"}),
+// 			"f2": sdata.NewStringMapFrom(map[string]interface{}{"a": "b"}),
+// 		}),
+// 	}
 
-	// expected
-	e := simpleType{
-		[]string{"b", "c", "a", "b"},
-		"a",
-		[]simpleSubType{simpleSubType{"c"}, simpleSubType{"d"}, simpleSubType{"a"}, simpleSubType{"b"}},
-		map[string]string{"a": "b", "c": "d", "e": "f"},
-		map[string]simpleSubType{
-			"a": simpleSubType{"b"},
-			"c": simpleSubType{"d"},
-			"e": simpleSubType{"f"},
-		},
-		&simpleSubType{"a"},
-		map[string]*simpleSubType{
-			"a": &simpleSubType{"b"},
-			"c": &simpleSubType{"d"},
-			"e": &simpleSubType{"f"},
-		},
-		sdata.NewStringMapFrom(map[string]interface{}{
-			"a":  "b",
-			"c":  "d",
-			"f1": sdata.NewStringMapFrom(map[string]interface{}{"a": "b"}),
-			"f2": sdata.NewStringMapFrom(map[string]interface{}{"a": "b"}),
-		}),
-	}
+// 	// expected
+// 	e := simpleType{
+// 		[]string{"b", "c", "a", "b"},
+// 		"a",
+// 		[]simpleSubType{simpleSubType{"c"}, simpleSubType{"d"}, simpleSubType{"a"}, simpleSubType{"b"}},
+// 		map[string]string{"a": "b", "c": "d", "e": "f"},
+// 		map[string]simpleSubType{
+// 			"a": simpleSubType{"b"},
+// 			"c": simpleSubType{"d"},
+// 			"e": simpleSubType{"f"},
+// 		},
+// 		&simpleSubType{"a"},
+// 		map[string]*simpleSubType{
+// 			"a": &simpleSubType{"b"},
+// 			"c": &simpleSubType{"d"},
+// 			"e": &simpleSubType{"f"},
+// 		},
+// 		sdata.NewStringMapFrom(map[string]interface{}{
+// 			"a":  "b",
+// 			"c":  "d",
+// 			"f1": sdata.NewStringMapFrom(map[string]interface{}{"a": "b"}),
+// 			"f2": sdata.NewStringMapFrom(map[string]interface{}{"a": "b"}),
+// 		}),
+// 	}
 
-	AppendOrReplace(&d, s)
+// 	AppendOrReplace(&d, s)
 
-	if !reflect.DeepEqual(d, e) {
-		t.FailNow()
-	}
-}
+// 	if !reflect.DeepEqual(d, e) {
+// 		t.FailNow()
+// 	}
+// }
+
+// type s map[string]interface{}
+
+// func TestReplaceMapMap(t *testing.T) {
+// 	src := s{"A": map[string]interface{}{
+// 		"a": map[string]interface{}{
+// 			"a1": "1",
+// 			"a2": "2",
+// 		},
+// 		"b": sdata.NewStringMapFrom(map[string]interface{}{
+// 			"b1": "1",
+// 			"b2": "2",
+// 		}),
+// 		"c": []string{"k", "d"},
+// 	}}
+
+// 	dst := s{"A": map[string]interface{}{
+// 		"a": map[string]interface{}{
+// 			"a1": "4",
+// 			"a2": "5",
+// 			"a3": "3",
+// 		},
+// 		"b": sdata.NewStringMapFrom(map[string]interface{}{
+// 			// "b1": "4",
+// 			// "b2": "5",
+// 			"b3": "3",
+// 		}),
+// 		"c": []string{"a", "b", "c"},
+// 	}}
+
+// 	exp := s{"A": map[string]interface{}{
+// 		"a": map[string]interface{}{
+// 			"a1": "1",
+// 			"a2": "2",
+// 			"a3": "3",
+// 		},
+// 		"b": sdata.NewStringMapFrom(map[string]interface{}{
+// 			"b1": "1",
+// 			"b2": "2",
+// 			"b3": "3",
+// 		}),
+// 		"c": []string{"a", "b", "c", "k", "d"},
+// 	}}
+
+// 	Merge(&dst, src)
+
+// 	t.Logf("%#v", dst["A"].(map[string]interface{})["c"])
+
+// 	if !reflect.DeepEqual(dst, exp) {
+// 		t.FailNow()
+// 	}
+// }
